a56bcd5c6e4849105172d4f0bee3a46070072fcf
Rojit <rojit.blaze13@gmail.com>
2022-10-18 13:28:59 +0545

    Add eslint setup for server (#18)

--- server/src/server.js
+++ server/src/server.js
@@ -8,14 +8,14 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  return res.json({ success: true, message: "Well done. Nice Catch." });
+	return res.json({ success: true, message: "Well done. Nice Catch." });
 });
 
 app.get("*", (req, res) => {
-  return res.json({ success: false, message: "Route not configured yet." });
+	return res.json({ success: false, message: "Route not configured yet." });
 });
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () =>
-  console.log(`Server is running in: http://localhost:${PORT}`)
+	console.log(`Server is running in: http://localhost:${PORT}`)
 );
