23141600cb5be03b724dacfe8889eda08c1a0ab1
Perfectizihirwe <perfectizihirwe@gmail.com>
2022-10-22 10:07:52 +0200

    Landing page (#37)
    * Landing page
    
    * Update testing
    
    * Update testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,6 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders documentation", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
