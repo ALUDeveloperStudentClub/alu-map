99a116fc04bd0564c4e4bdd9487836606f09d4f5
Perfectizihirwe <perfectizihirwe@gmail.com>
2022-10-22 02:08:12 +0200

    Update testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders documentation", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Documentation/i);
   expect(linkElement).toBeInTheDocument();
 });
