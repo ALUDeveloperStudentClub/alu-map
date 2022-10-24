65f558d250e599f9f11a271a24e7ef9068decf2e
Dirac Murairi <dmurairimukongya@gmail.com>
2022-10-24 18:57:18 +0200

    add routing into the project

--- client/src/App.js
+++ client/src/App.js
@@ -1,93 +1,28 @@
+// use lazy loading of components
+import React, { lazy, Suspense } from "react";
+import { Routes, BrowserRouter, Route } from "react-router-dom";
+import StatesProvider from "./states/states";
+
 import "./App.css";
+
+// add pages
+const Contributors = lazy(() => import("./pages/contributors.page"));
+
+
+
 
 function App() {
   return (
-    <div
-      className="App"
-      id="root"
-      style={{
-        backgroundImage: `url("https://i.ibb.co/DkLQ1zD/NR-footer-bg.jpg")`,
-        backgroundSize: "cover",
-      }}
-    >
-      <header id="header">
-        <div id="logoContainer">
-          <img
-            src={require("./assets/ALU-logo-white-full.png")}
-            alt="logo"
-            id="logo"
-          />
-        </div>
-        <a
-          href="https://github.com/ALUDeveloperStudentClub/alu-map"
-          target="_blank"
-          rel="noreferrer"
-          id="websiteLink"
-        >
-          GO TO GITHUB REPO
-        </a>
-      </header>
-      <main id="body">
-        <h3>WELCOME TO ALU MAP DEVELOPMENT</h3>
-        <span>Documetation</span>
-        <ul>
-          <li>
-            If you haven't already run, npm install to install node modules
-          </li>
-          <li>
-            Install{" "}
-            <a
-              href="https://www.docker.com/"
-              target="_blank"
-              rel="noreferrer"
-              style={{
-                color: "orange",
-              }}
-            >
-              Docker
-            </a>{" "}
-            Desktop for Windows, Linux or Mac, and run it. If you are using
-            Windows, make sure you have enabled{" "}
-            <a
-              href="https://learn.microsoft.com/en-us/virtualization/hyper-v-on-windows/quick-start/enable-hyper-v"
-              target={"_blank"}
-              rel="noreferrer"
-              style={{
-                color: "orange",
-              }}
-            >
-              Hyper-V
-            </a>{" "}
-            and Virtualization Technology in your BIOS.
-          </li>
-          <li>
-            Run docker-compose up --build to start the containers. This will
-            take a few minutes the first time you run it.
-          </li>
-          <li>
-            Run npm start to start the development server. The server will run
-            on port 8080.
-          </li>
-          <li>
-            Check the list of{" "}
-            <a
-              href="https://github.com/ALUDeveloperStudentClub/alu-map/issues"
-              rel="noreferrer"
-              target={"_blank"}
-              style={{
-                color: "orange",
-              }}
-            >
-              issues
-            </a>{" "}
-            on github and pick one to work on (Be sure to communicate it through
-            a comment). If you have any questions, contact the repo organiser
-            (Dirac156).
-          </li>
-        </ul>
-        <span>HAPPY CODING🤗 ~ Perfect Gift IZIHIRWE</span>
-      </main>
-    </div>
+      <StatesProvider>
+        <Suspense fallback={<div>Loading...</div>}>
+          <BrowserRouter>
+            <Routes>
+              <Route exact path="/" element={<div>Home</div>} />
+              <Route exact path="/contribution" element={<Contributors />} />
+            </Routes>
+          </BrowserRouter>
+        </Suspense>
+      </StatesProvider>
   );
 }
 
